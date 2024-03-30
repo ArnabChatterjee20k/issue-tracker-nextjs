e94cb9803ad7e777a82186273a87b294249d8972
arnabchatterjee <arnabchatterjee.ac.2@gmail.com>
2024-03-30 22:08:19 +0530

    feat -- added navbar

---
+++ src/app/header.tsx
@@ -0,0 +1,32 @@
+"use client"
+import React from "react";
+import { FaBug } from "react-icons/fa6";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+const links = [
+  { link: "/dashboard", name: "Dashboard" },
+  { link: "/issues", name: "Issues" },
+];
+export default function Header() {
+  const pathname = usePathname();
+
+  return (
+    <nav className="flex px-6 h-14 items-center space-x-6 border border-b">
+      <Link href="/">
+        <FaBug />
+      </Link>
+      <ul className="space-x-5 flex">
+        {links.map(({ link, name }) => (
+          <Link
+            href={link}
+            className={`${
+              pathname === link ? "text-gray-900" : "text-gray-600"
+            }  hover:text-gray-900 font-medium transition-colors ease-in`}
+          >
+            {name}
+          </Link>
+        ))}
+      </ul>
+    </nav>
+  );
+}
