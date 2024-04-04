71ef72a33c3aaf1d54e38e69f9ebf327cb280a4d
arnabchatterjee <arnabchatterjee.ac.2@gmail.com>
2024-04-04 21:46:56 +0530

    feat -- added redirection when accessing auth form if logged in

--- src/middleware.ts
+++ src/middleware.ts
@@ -5,15 +5,19 @@ import { cookies } from "next/headers";
 import { SessionData, sessionOptions } from "./lib/authentication";
 export async function middleware(request: NextRequest) {
   // If the user is authenticated, continue as normal
-  const session = await getIronSession<SessionData>(cookies(),sessionOptions)
-  if (session.isLoggedIn) {
-    return NextResponse.next();
-  }
+  const session = await getIronSession<SessionData>(cookies(), sessionOptions);
+  const path = request.nextUrl.pathname;
+
+  const isAuthPath = path === "/login" || path === "/register";
+
+  if (session.isLoggedIn && isAuthPath)
+    return NextResponse.redirect(new URL("/", request.url));
+  if (session.isLoggedIn) return NextResponse.next();
 
   // Redirect to login page if not authenticated
   return NextResponse.redirect(new URL("/login", request.url));
 }
 
 export const config = {
-  matcher: ["/dashboard","/issues"],
+  matcher: ["/dashboard", "/issues", "/login", "/register"],
 };
